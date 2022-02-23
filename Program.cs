6b7d0bdb09568588b113941aae0bdf7abff39c57
Manav Misra <manavm@visionwebsoft.com>
2022-02-23 08:01:33 -0600

    :tada: Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace RockPaperScissors;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Rock, Paper, Scissors!");
+    }
+}
